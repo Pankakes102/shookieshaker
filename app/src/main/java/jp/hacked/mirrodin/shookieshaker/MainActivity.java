90c6e532d454ddf52f855495687d57bc7579f257
Andy Jolly <jollymanster@gmail.com>
2016-03-28 13:51:44 -0400

    Fixed any errors from previous commits

--- app/src/main/java/jp/hacked/mirrodin/shookieshaker/MainActivity.java
+++ app/src/main/java/jp/hacked/mirrodin/shookieshaker/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // Intent that call PlayActivity.class
-        Button playBut= (Button) findViewById(R.id.playButton);
+        Button playBut = (Button) findViewById(R.id.playButton);
         playBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
         });
 
         // Intent that call PlayActivity.class
-        Button leaderboardBut= (Button) findViewById(R.id.leaderboardButton);
+        Button leaderboardBut = (Button) findViewById(R.id.leaderboardButton);
         leaderboardBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,47 +41,4 @@ public class MainActivity extends Activity {
         });
 
     }
-
-
-<<<<<<< Updated upstream
-=======
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    public void alert(View view){
-        System.out.println("SCORE: " + score);
-        //System.out.format("SCORE: %d",score);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Add to high score list?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        //System.out.format("SCORE: %d", score);
-                        FirebaseClass DB_Update = new FirebaseClass();
-                        try {
-                            DB_Update.Update_DB(score);
-                            Toast.makeText(getApplicationContext(), "High Score Successfully added",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                        catch(Exception e){
-
-                            Toast.makeText(getApplicationContext(), "Could not add high score to database",
-                                    Toast.LENGTH_LONG).show();
-
-                        }
-
-
-
-                        // DB_Update.Update_DB(score);
-                        dialog.cancel();
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
-    }
->>>>>>> Stashed changes
 }
